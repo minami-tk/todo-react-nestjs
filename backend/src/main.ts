1883ba71929ad9d10d4366a5cf434ed888c3eab7
mtakashiba <mtakashiba@fab.pasona.tech>
2022-07-08 22:25:56 +0900

    change port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  await app.listen(5001);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
   });
 }
 bootstrap();
--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://localhost:5001/api/v1",
   headers: ({
     Authorization: "Bearer " + localStorage.getItem("token")
   })
